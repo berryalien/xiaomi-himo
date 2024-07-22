4fb35c6895aa8f565fd79a16735c558efa5abc6a
Christina <150205323+berryalien@users.noreply.github.com>
2024-07-23 01:56:21 +0400

    Smooth scrolling after clicking on an anchor link

--- js/main.js
+++ js/main.js
@@ -7,4 +7,12 @@ $('.bike-slider, .slider__items').slick({
   autoplaySpeed: 2000  
 });
 
+    $(".menu, .bike__column").on("click", "a", function (event) {
+      event.preventDefault();
+      var id = $(this).attr('href'),    
+      top = $(id).offset().top;
+      $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+
 });
